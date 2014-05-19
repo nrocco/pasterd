9deba83bd0061321ad6c5c9c373d6aee4fa6757b
Nico Di Rocco <dirocco.nico@gmail.com>
2014-05-19 21:54:08 +0200

    Add support for uploading images

--- pasterd/webserver.py
+++ pasterd/webserver.py
@@ -2,6 +2,9 @@
 import logging
 import os
 import random
+import base64
+
+from mimetypes import MimeTypes
 from datetime import datetime
 
 import bottle
@@ -62,32 +65,51 @@ def index():
 @bottle.route('/', method='POST', apply=[respond_in_plaintext, catch_exceptions])
 def make_paste(db):
     db.text_factory = str
-    paste = bottle.request.params.get(VAR)
 
-    if not paste:
+    if VAR in bottle.request.files:
+        upload = bottle.request.files.get(VAR)
+        name, ext = os.path.splitext(upload.filename)
+
+        if ext not in ('.png','.jpg','.jpeg'):
+            raise Exception('File extension not allowed.')
+
+        mime = MimeTypes()
+        mime_type = mime.guess_type(upload.filename)
+        content_type = mime_type[0]
+        paste = base64.b64encode(upload.file.read())
+    elif VAR in bottle.request.params:
+        content_type = "text/plain"
+        paste = bottle.request.params.get(VAR)
+    else:
         raise Exception('Usage: send %s POST variable' % VAR)
 
     ip = bottle.request.get('REMOTE_ADDR')
     now = datetime.now()
     paste_id = generate_rand()
 
-    c = db.execute('INSERT INTO pastes VALUES (?,?,?,?)',
-                   (paste_id, ip, now, paste))
+    c = db.execute('INSERT INTO pastes VALUES (?,?,?,?,?)',
+                   (paste_id, ip, now, content_type, paste))
     db.commit()
 
     return '%s/%s\n' % (URL, paste_id)
 
 
-@bottle.route('/:paste_id', method='GET', apply=[respond_in_plaintext, catch_exceptions])
+@bottle.route('/:paste_id', method='GET', apply=[catch_exceptions])
 def show_paste(db, paste_id):
-    c = db.execute('SELECT content FROM pastes WHERE id = ?', (paste_id,))
+    c = db.execute('SELECT type, content FROM pastes WHERE id = ?', (paste_id,))
     paste = c.fetchone()
 
     if not paste:
         bottle.response.status= 404
         return 'Paste %s does not exist' % paste_id
 
-    return '%s' % paste['content']
+    if "text/plain" != paste['type']:
+        content = base64.b64decode(paste['content'])
+    else:
+        content = paste['content']
+
+    bottle.response.content_type = paste['type']
+    return '%s' % content
 
 
 #
@@ -106,7 +128,7 @@ def setup(db, base_url=URL, paste_variable=VAR):
         import sqlite3
         conn = sqlite3.connect(db)
         c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS pastes (id VARCHAR(8) UNIQUE, ip VARCHAR(15), created VARCHAR(26), content TEXT);''')
+        c.execute('''CREATE TABLE IF NOT EXISTS pastes (id VARCHAR(8) UNIQUE, ip VARCHAR(15), created VARCHAR(26), type VARCHAR(50), content TEXT);''')
         conn.commit()
         conn.close()
     bottle.install(SQLitePlugin(dbfile=db))
